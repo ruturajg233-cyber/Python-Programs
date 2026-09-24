84bea7fd4427f8abb0ce29e65e72f0ffaf3743d7
Chandru <ruturajg233@gmail.com>
2026-09-24 11:25:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+
+
+
+
+print("hello")
+
+name = "sridhun"
+age = 20
+balance = 20987.23
+
+print(f"{'name:':<12}{name}")
+print(f"{'age:':<12}{age}")
+print(f"{'balance:':<12}{balance}")
+
+x = [1, 2, 3]  
+print(id(x))
+
+x[0] = 4
+print(x)
